bbbfa4a9dd7c70339bba183b4b0451ebd9d698f6
unknown <wayfairers.co@gmail.com>
2016-06-26 16:52:45 +1000

    forms with new version

--- src/app/new-todo/new-todo.component.ts
+++ src/app/new-todo/new-todo.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder,Validators,FormGroup} from '@angular/forms';
 
 @Component({
   moduleId: module.id,
@@ -7,10 +8,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['new-todo.component.css']
 })
 export class NewTodoComponent implements OnInit {
-
-  constructor() {}
+  myForm:FormGroup; 
+  constructor(private formBuilder:FormBuilder) {}
 
   ngOnInit() {
+    this.myForm =  this.formBuilder.group({
+      todoTitle:['',Validators.required] 
+    });
   }
 
 }
--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,15 @@ import { bootstrap } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 import { AppComponent, environment } from './app/';
 import {HTTP_PROVIDERS} from '@angular/http';
+import { disableDeprecatedForms, provideForms } from '@angular/forms';
 
 if (environment.production) {
   enableProdMode();
 }
 
-bootstrap(AppComponent,[HTTP_PROVIDERS]);
+bootstrap(AppComponent,[
+  HTTP_PROVIDERS,
+  disableDeprecatedForms(),
+  provideForms()
+  ]);
 
